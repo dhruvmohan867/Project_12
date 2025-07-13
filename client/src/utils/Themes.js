2c9162e880ed594ab86e1f5376cc19ade9b79d81
dhruvmohan867 <dhruvmohanshukla@gmail.com>
2025-07-14 02:58:07 +0530

    error

--- client/src/utils/Themes.js
+++ client/src/utils/Themes.js
@@ -1,60 +1,28 @@
-import styled, { ThemeProvider } from "styled-components";
-// import { lightTheme } from ".Themes.js"; // ✅ Uncommented and fixed
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+export const lightTheme = {
+  bg: "#FFFFFF",
+  bgLight: "#FFFFFF",
+  primary: "#131118",
+  secondary: "#5B86E5",
 
-import Navbar from "../components/Navbar.jsx"; // ✅ Case-sensitive and correct path
-import Home from "../pages/Home.jsx";
-import Authentication from "../pages/Authentication.jsx";
-import ShopListing from "../pages/ShopListing.jsx";
-import Favourite from "../pages/Favourite.jsx";
-import Cart from "../pages/Cart.jsx";
-import ProductDetails from "../pages/ProductDetails.jsx";
-import ToastMessage from "../components/ToastMessage.jsx";
-
-import { useDispatch, useSelector } from "react-redux";
-import { useState } from "react";
-
-const Container = styled.div`
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  background: ${({ theme }) => theme.bg};
-  color: ${({ theme }) => theme.text_primary};
-  overflow-x: hidden;
-  overflow-y: hidden;
-  transition: all 0.2s ease;
-`;
-
-function App() {
-  const { currentUser, open, message, severity } = useSelector((state) => state.user); // ✅ Combined selector
-  const [openAuth, setOpenAuth] = useState(false);
-
-  return (
-    <ThemeProvider theme={lightTheme}>
-      <BrowserRouter>
-        <Container>
-          <Navbar setOpenAuth={setOpenAuth} currentUser={currentUser} />
-
-          <Routes>
-            <Route path="/" element={<Home />} />                {/* ✅ Removed exact */}
-            <Route path="/shop" element={<ShopListing />} />
-            <Route path="/favorite" element={<Favourite />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/shop/:id" element={<ProductDetails />} />
-          </Routes>
-
-          {openAuth && (
-            <Authentication openAuth={openAuth} setOpenAuth={setOpenAuth} />
-          )}
-
-          {open && (
-            <ToastMessage open={open} message={message} severity={severity} />
-          )}
-        </Container>
-      </BrowserRouter>
-    </ThemeProvider>
-  );
-}
-
-export default App;
+  disabled: "#b1b2b3",
+  menubar: "#191c29",
+  navbar: "#242B3F",
+  arrow: "#AFAFB5",
+  menu_primary_text: "#F2F3F4",
+  menu_secondary_text: "#b1b2b3",
+  table_header: "#242445",
+  text_primary: "#404040",
+  text_secondary: "#4d4c4c",
+  card: "#FFFFFF",
+  black: "#000000",
+  white: "#FFFFFF",
+  shadow: "#00000020",
+  green: "#00ff6a",
+  yellow: "#e8ba00",
+  red: "#ef5350",
+  orange: "#F7AD63",
+  popup: "#242B3F",
+  popup_text_primary: "#F2F3F4",
+  popup_text_secondary: "#b1b2b3",
+  output_node: "#49516b",
+};
